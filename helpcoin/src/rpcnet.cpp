a0f7c44448227bb3cd63ab43285e549c582805bc
martyn_luter <martyn_luter@ukr.net>
2014-04-02 21:00:55 +0300

    Qt for windows succesfully built

--- helpcoin/src/rpcnet.cpp
+++ helpcoin/src/rpcnet.cpp
@@ -3,6 +3,7 @@
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
+#include <winsock2.h>
 #include "net.h"
 #include "bitcoinrpc.h"
 
